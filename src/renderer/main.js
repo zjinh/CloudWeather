5678a4d2834cee0195157034157999e9135320de
ZJINH <2665229856@qq.com>
2018-07-03 19:37:26 +0800

    修改组件引入为按需引入，减小项目体积

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -4,13 +4,17 @@ import axios from 'axios'
 import App from './App'
 import router from './router'
 import store from './store'
-import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-Vue.use(ElementUI);
+import { MessageBox,Message } from 'element-ui';
 
+//Vue.use(MessageBox);
+
+Vue.prototype.$prompt = MessageBox.prompt;
+Vue.prototype.$message=Message;
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
 Vue.http = Vue.prototype.$http = axios;
 Vue.config.productionTip = false;
+
 /* eslint-disable no-new */
 new Vue({
   components: { App },
